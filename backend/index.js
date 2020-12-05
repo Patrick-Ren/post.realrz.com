ccd7d00e53cdb022e0bdb0d653a5f4da1f4ecef5
rz <rz19960718@gmail.com>
2020-12-05 18:29:06 +0800

    TBC

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,57 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const Datastore = require("nedb");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// load database
+const database = new Datastore({ filename: "posts.db", autoload: true });
+
+// init express app
+const port = 4000;
+const app = express();
+app.use(express.json()); // middleware for parsing application/json
+
+// admin login
+app.post("/auth", (req, res) => {
+  const { key } = req.body;
+  if (key === "Apple1996") {
+    res.status(200).end();
+  } else {
+    res.status(401).end(); // 401 for not authorized
+  }
 });
 
+// read all posts
+app.get("/posts", (req, res) => {
+  // query database
+  database.find({}, (err, data) => {
+    if (err) {
+      console.log("find failed");
+      res.status(500).end();
+    }
+    console.log(data);
+    console.log(typeof data);
+    res.send(data);
+    res.status(200).end();
+  });
+});
+
+// read post details
+app.get("/posts/:id", (req, res) => {
+  res.send(`you are reading post ${req.params.id}`);
+  res.status(200).end();
+});
+
+// create new post
+app.post("/newpost", (req, res) => {});
+
+// update post
+app.put("/posts/:id", (req, res) => {
+  const { id, title, date, topic } = req.body;
+  //
+});
+
+// delete post
+app.delete("/posts/:id", (req, res) => {});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`node app running at http://localhost:${port}`);
 });
